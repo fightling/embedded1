8f276ded2533fac9bfacb4452657482b48d915e1
Markus Schubert <markus@toto.io>
2024-04-03 20:02:50 +0200

    adding stuff for max7219

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,47 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
-use esp_println::println;
+use esp_hal::{
+    clock::ClockControl,
+    gpio::{self, Event, Input, PullDown, IO, GpioPin},
+    peripherals::Peripherals,
+    prelude::*,
+    spi::{master::Spi, SpiMode},
+    Delay,
+};
+// use esp_println::println;
+// use embedded_hal::digital::v2::OutputPin;
+use max7219::*;
 
 #[entry]
 fn main() -> ! {
+
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
 
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let mut delay = Delay::new(&clocks);
+    let mut io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    println!("Hello world!");
-    loop {
-        println!("Loop...");
-        delay.delay_ms(500u32);
-    }
+    let mut sck_pin = io.pins.gpio6.into();
+    let mut mosi_pin = io.pins.gpio7.into();
+    let mut cs_pin = io.pins.gpio10.into();
+
+    let mut display = MAX7219::from_pins(1, mosi_pin, cs_pin, sck_pin).unwrap();
+
+    // make sure to wake the display up
+    display.power_on().unwrap();
+    // write given octet of ASCII characters with dots specified by 3rd param bits
+    display.write_str(0, b"pls help", 0b00100000).unwrap();
+    // set display intensity lower
+    display.set_intensity(0, 0x1).unwrap();
+
+    loop {}
+
+    // let clocks = ClockControl::max(system.clock_control).freeze();
+    // let mut delay = Delay::new(&clocks);
+    //
+    // println!("Hello world!");
+    // loop {
+    //     println!("Loop...");
+    //     delay.delay_ms(500u32);
+    // }
 }
