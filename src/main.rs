8fe821e5b1573714149da59b5b00bec9da53f417
Patrick Hoffmann <p.hoffmann@berlin.de>
2024-04-26 10:09:00 +0200

    implement blinking LED

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,26 @@
 #![no_main]
 
 use esp_backtrace as _;
-use esp_hal::{gpio::IO, peripherals::Peripherals, prelude::*};
-// use esp_println::println;
-// use embedded_hal::digital::v2::OutputPin;
-use max7219::*;
+use esp_hal::{clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, Delay};
+use esp_println::println;
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
-    // let system = peripherals.SYSTEM.split();
-
+    let system = peripherals.SYSTEM.split();
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    let sck_pin = io.pins.gpio6.into_push_pull_output();
-    let mosi_pin = io.pins.gpio7.into_push_pull_output();
-    let cs_pin = io.pins.gpio10.into_push_pull_output();
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
 
-    let mut display = MAX7219::from_pins(1, mosi_pin, cs_pin, sck_pin).unwrap();
+    let mut led = io.pins.gpio7.into_push_pull_output();
 
-    // make sure to wake the display up
-    display.power_on().unwrap();
-    // write given octet of ASCII characters with dots specified by 3rd param bits
-    display.write_str(0, b"pls help", 0b00100000).unwrap();
-    // set display intensity lower
-    display.set_intensity(0, 0x1).unwrap();
-
-    loop {}
-
-    // let clocks = ClockControl::max(system.clock_control).freeze();
-    // let mut delay = Delay::new(&clocks);
-    //
-    // println!("Hello world!");
-    // loop {
-    //     println!("Loop...");
-    //     delay.delay_ms(500u32);
-    // }
+    println!("Hello world!");
+    loop {
+        println!("Loop...");
+        delay.delay_ms(500u32);
+        led.set_high().unwrap();
+        delay.delay_ms(500u32);
+        led.set_low().unwrap();
+    }
 }
